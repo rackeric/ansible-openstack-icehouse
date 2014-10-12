74e84f7a37a5f34da0d38b8caa7dfe4b3ed11b6a
rackeric <eric.hernandez@rackspace.com>
2014-10-12 03:53:44 -0500

    broke off glance tests to glance_tests.sh

--- scripts/glance.sh
+++ scripts/glance.sh
@@ -41,21 +41,3 @@ keystone service-create --name=glance --type=image --description="OpenStack Imag
 # Step 8
 service openstack-glance-api start;service openstack-glance-registry start;chkconfig openstack-glance-api on;chkconfig openstack-glance-registry on;
 
-
-## VERIFY ##
-
-# Step 1
-mkdir /tmp/images;cd /tmp/images/;wget http://cdn.download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img;
-
-# Step 2
-glance image-create --name "cirros-0.3.2-x86_64" --disk-format qcow2 --container-format bare --is-public True --progress < cirros-0.3.2-x86_64-disk.img
-
-# Step 3
-glance image-list
-Step 4.
-rm -r /tmp/images
-
-## CIRROS VHD ##
-curl -O https://github.com/downloads/citrix-openstack/warehouse/cirros-0.3.0-x86_64-disk.vhd.tgz;
-glance image-create --name cirros-vhd --container-format=ovf --disk-format=vhd < cirros-0.3.0-x86_64-disk.vhd.tgz;
-rm cirros*.tgz;
---
+++ scripts/glance_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+## VERIFY GLANCE ##
+
+export OS_SERVICE_TOKEN=a1s2d3f4g5h6j7k8
+export OS_SERVICE_ENDPOINT=http://controller:35357/v2.0
+
+# Step 1
+mkdir /tmp/images;
+cd /tmp/images/;
+wget http://cdn.download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img;
+
+# Step 2
+glance image-create --name "cirros-0.3.2-x86_64" --disk-format qcow2 --container-format bare --is-public True --progress < cirros-0.3.2-x86_64-disk.img
+
+# Step 3
+glance image-list
+
+# Step 4
+rm -r /tmp/images
+
+## CIRROS VHD ##
+curl -O https://github.com/downloads/citrix-openstack/warehouse/cirros-0.3.0-x86_64-disk.vhd.tgz;
+glance image-create --name cirros-vhd --container-format=ovf --disk-format=vhd < cirros-0.3.0-x86_64-disk.vhd.tgz;
+rm cirros*.tgz;
