5ea9ec43f4d5295ff8b4157bae2270bc159aeeda
rackeric <eric.hernandez@rackspace.com>
2014-10-18 12:53:10 -0500

    added symlink to custom built gmp lib

--- roles/glance/files/glance_db_workaround.sh
+++ roles/glance/files/glance_db_workaround.sh
@@ -6,7 +6,7 @@ yum -y install gcc libgcc glibc libffi-devel libxml2-devel libxslt-devel zlib-de
 
 wget https://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.bz2
 tar -xvjpf gmp*
-cd gmp*
+cd gmp-6.0.0
 ./configure
 make
 make check
@@ -16,6 +16,8 @@ make install
 
 mkdir ~/back
 mv /usr/lib64/libgmp* ~/back/
-cp /usr/local/lib/libgmp* /usr/lib64/
+#cp /usr/local/lib/libgmp* /usr/lib64/
+ln -s /usr/local/lib/libgmp.so.10.2.0 /usr/lib64/libgmp.so
+ln -s /usr/local/lib/libgmp.so.10.2.0 /usr/lib64/libgmp.so.10
 cd ~
 rm -Rf ~/gmp*
