590e0a801c579f2a5be7313e60ac8baac8a3f9c8
rackeric <eric.hernandez@rackspace.com>
2014-10-19 01:25:41 -0500

    updated keystone.sh for vars for controller

--- roles/keystone/files/keystone.sh
+++ roles/keystone/files/keystone.sh
@@ -4,9 +4,10 @@
 
 ADMIN_PASS="admin"
 DEMO_PASS="demo"
+CONTROLLER="controller"
 
 export OS_SERVICE_TOKEN=a1s2d3f4g5h6j7k8
-export OS_SERVICE_ENDPOINT=http://controller:35357/v2.0
+export OS_SERVICE_ENDPOINT=http://$CONTROLLER:35357/v2.0
 
 keystone user-create --name=admin --pass=$ADMIN_PASS --email=admin@domain.com
 keystone role-create --name=admin
@@ -20,4 +21,4 @@ keystone user-role-add --user=demo --role=_member_ --tenant=demo
 keystone tenant-create --name=service --description="Service Tenant"
 
 keystone service-create --name=keystone --type=identity   --description="OpenStack Identity"
-keystone endpoint-create   --service-id=$(keystone service-list | awk '/ identity / {print $2}')   --publicurl=http://controller:5000/v2.0   --internalurl=http://controller:5000/v2.0   --adminurl=http://controller:35357/v2.0
+keystone endpoint-create   --service-id=$(keystone service-list | awk '/ identity / {print $2}')   --publicurl=http://$CONTROLLER:5000/v2.0   --internalurl=http://$CONTROLLER:5000/v2.0   --adminurl=http://$CONTROLLER:35357/v2.0
