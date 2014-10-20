b2df2e8b6b8390487fd5b6d68f5cbe9437ff24fc
rackeric <eric.hernandez@rackspace.com>
2014-10-20 01:33:02 -0500

    added conditional to see if gmp directory exists so not to compile again

--- roles/glance/files/glance_db_workaround.sh
+++ roles/glance/files/glance_db_workaround.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Fix for glance db sync fail for gmp
 
+if [ -d "gmp-6.0.0" ]; then
+  exit
+fi
+
 yum -y groupinstall "Development tools";
 yum -y install gcc libgcc glibc libffi-devel libxml2-devel libxslt-devel zlib-devel bzip2-devel ncurses-devel python-devel; # openssl-devel was not working
 
@@ -19,5 +23,3 @@ mv /usr/lib64/libgmp* ~/back/
 #cp /usr/local/lib/libgmp* /usr/lib64/
 ln -s /usr/local/lib/libgmp.so.10.2.0 /usr/lib64/libgmp.so
 ln -s /usr/local/lib/libgmp.so.10.2.0 /usr/lib64/libgmp.so.10
-cd ~
-rm -Rf ~/gmp*
